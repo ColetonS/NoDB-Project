6ead3a18b56da3a7b53c4d9794ad53a0f7622a0a
ColetonS <coleton.m.smith@gmail.com>
2019-07-21 22:13:33 -0600

    beefed up data

--- server/data.js
+++ server/data.js
@@ -118,6 +118,132 @@ const data = [
         title: 'Yakisoba',
         img: 'https://www.justonecookbook.com/wp-content/uploads/2017/02/Yakisoba-NEW-II.jpg',
         cuisine: 'Japanese'
+    },
+    {
+        id: 22,
+        title: 'Bulgogi',
+        img: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2019/5/15/0/FNK_Korean-Grilled-Beef-Bulgogi-H2_s4x3.jpg.rend.hgtvcom.826.620.suffix/1557961246095.jpeg',
+        cuisine: 'Korean'
+    },
+    {
+        id: 23,
+        title: 'Bibimbap',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Dolsot-bibimbap.jpg/1200px-Dolsot-bibimbap.jpg',
+        cuisine: 'Korean'
+    },
+    {
+        id: 24,
+        title: 'Japchae',
+        img: 'https://www.jessicagavin.com/wp-content/uploads/2016/06/japchae-korean-glass-noodles-with-tofu-and-vegetables-1200.jpg',
+        cuisine: 'Korean'
+    },
+    {
+        id: 25,
+        title: 'Samgyeopsal',
+        img: 'https://www.maangchi.com/wp-content/uploads/2009/09/grilled-porkbelly_cut1.jpg',
+        cuisine: 'Korean'
+    },
+    {
+        id: 26,
+        title: 'Bratwurst',
+        img: 'https://9woclymefe-flywheel.netdna-ssl.com/wp-content/uploads/Plated-Cherry-Brats.jpg',
+        cuisine: 'German'
+    },
+    {
+        id: 27,
+        title: 'Schnitzel',
+        img: 'https://www.thespruceeats.com/thmb/_lj5FMmlXhjUGcvPu0dgc6_WTow=/450x0/filters:no_upscale():max_bytes(150000):strip_icc()/wiener-schnitzel-recipe-1447089-Hero-5b587d6c46e0fb0071b0059d.jpg',
+        cuisine: 'German'
+    },
+    {
+        id: 28,
+        title: 'Chow Mein',
+        img: 'https://www.recipetineats.com/wp-content/uploads/2016/07/Chow-Mein_9.jpg',
+        cuisine: 'Chinese'
+    },
+    {
+        id: 29,
+        title: 'Orange Chicken',
+        img: 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/8/26/3/WU0802H_Orange-Chicken_s4x3.jpg.rend.hgtvcom.826.620.suffix/1440693433689.jpeg',
+        cuisine: 'Chinese'
+    },
+    {
+        id: 30,
+        title: 'Peking Duck',
+        img: 'https://www.curiouscuisiniere.com/wp-content/uploads/2018/10/Peking-Duck-7206-1.jpg',
+        cuisine: 'Chinese'
+    },
+    {
+        id: 31,
+        title: 'Sweet and Sour Pork',
+        img: 'https://tipbuzz.com/wp-content/uploads/Sweet-and-Sour-Pork-Thumbnail.jpg',
+        cuisine: 'Chinese'
+    },
+    {
+        id: 32,
+        title: 'Fried Catfish',
+        img: 'https://www.simplyrecipes.com/wp-content/uploads/2012/07/fried-catfish-vertical-a-1400.jpg',
+        cuisine: 'American'
+    },
+    {
+        id: 33,
+        title: 'King Crab Legs',
+        img: 'https://thestayathomechef.com/wp-content/uploads/2019/02/How-to-Cook-Crab-Legs-1-500x375.jpg',
+        cuisine: 'American'
+    },
+    {
+        id: 34,
+        title: 'Burnt Ends',
+        img: 'https://i2.wp.com/howtobbqright.com/wp-content/uploads/2017/08/RFI_burntends.jpg?fit=640%2C510&ssl=1',
+        cuisine: 'American'
+    },
+    {
+        id: 35,
+        title: 'Carne Asada',
+        img: 'https://pinchandswirl.com/wp-content/uploads/2016/08/Carne-Asada-thinly-sliced-across-the-grain-featured.jpg',
+        cuisine: 'Mexican'
+    },
+    {
+        id: 36,
+        title: 'Clam Chowder',
+        img: 'https://honest-food.net/wp-content/uploads/2012/05/maine-clam-chowder.jpg',
+        cuisine: 'American'
+    },
+    {
+        id: 37,
+        title: 'French Onion Soup',
+        img: 'https://www.simplyrecipes.com/wp-content/uploads/2015/02/french-onion-soup-horiz-a-1600.jpg',
+        cuisine: 'French'
+    },
+    {
+        id: 38,
+        title: 'Beef Bourguignon',
+        img: 'https://images-gmi-pmc.edge-generalmills.com/42fb4d81-8d92-40e2-9f0a-524edce5ca74.jpg',
+        cuisine: 'French'
+    },
+    {
+        id: 39,
+        title: 'Tandoori Chicken',
+        img: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/chicken-tandori-1526595014.jpg?resize=480:*',
+        cuisine: 'Indian'
+    },
+    {
+        id: 40,
+        title: 'Grilled Cheese',
+        img: 'https://cdn.craftbeer.com/wp-content/uploads/grilledcheese1-900x600.jpg',
+        cuisine: 'American'
+    },
+    {
+        id: 41,
+        title: 'Mac and Cheese',
+        img: 'https://sugarspunrun.com/wp-content/uploads/2018/01/baked-macaroni-and-cheese-recipe-1-of-1-12.jpg',
+        cuisine: 'American'
+    },
+    {
+        id: 42,
+        title: 'Bruschetta',
+        img: 'https://d1bjorv296jxfn.cloudfront.net/s3fs-public/styles/recipes_header_image/public/2-Bruschetta_Mozzarella.jpg?itok=wUNvjArC',
+        cuisine: 'Italian'
     }
 ]
 module.exports = data 
